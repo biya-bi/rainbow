7da7ccb76a7cc9b3ddcd84c7a257a872ea2195e5
Biya-Bi <biya-bi@hotmail.fr>
2017-02-12 22:07:44 +0100

    Improved photo support in products and categories.
    Now, the org.rainbow.shopping.cart.model.File class contains all the information about the file representing the photo.
    This is necessary for clients to correctly display the photo. Product and Category views (.xhtml files) have also been adjusted accordingly.

---
+++ rainbow-shopping-cart-ui-web/src/main/java/org/rainbow/shopping/cart/ui/web/utilities/BytesToImageConverter.java
@@ -0,0 +1,27 @@
+package org.rainbow.shopping.cart.ui.web.utilities;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.rainbow.shopping.cart.model.File;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BytesToImageConverter {
+
+	public StreamedContent getImage(File photo) throws IOException {
+		if (photo == null || photo.getFileContent() == null || photo.getFileContentType() == null
+				|| FacesContext.getCurrentInstance().getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+			return new DefaultStreamedContent();
+		} else {
+			return new DefaultStreamedContent(new ByteArrayInputStream(photo.getFileContent()),
+					photo.getFileContentType());
+		}
+	}
+
+}
